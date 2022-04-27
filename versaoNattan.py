b3d63a3c3b8b196f5e9a1d8fef8f8ab6552527d9
NaTTaNMendes <nattan078@gmail.com>
2022-04-27 11:09:59 -0300

    Finalização do conversor de binário para hamming

--- versaoNattan.py
+++ versaoNattan.py
@@ -1,4 +1,5 @@
-binario = "01100010"
+binario = "01100001"
+print("Binário informado:", binario)
 qtdBits = len(binario)
 
 if (qtdBits % 11 != 0): #transforma o código número em múltiplo de 11
@@ -25,38 +26,88 @@ while True:
         break
     divisao = binario[inicio : fim]
     matriz = "xxx" + divisao[0] + "x" + divisao[1:4] + "x" + divisao[4:11]
-    print(matriz)
     conjuntos.append(matriz)
     inicio = fim
     fim += 11
-'''
 
-FALTA OTIMIZAR ESSA PARTE DO CODIGO E COLOCAR DENTRO DE UM WHILE
+def criarQ1(binario): #verifica o bit necessário para as colunas 2 e 4
+    y = 1
+    um = 0
+    while (y < 16):
+        if (binario[y] != "x"):  
+            if (binario[y] == "1"):
+                um += 1
+        y += 2
+    if (um % 2 == 1):
+        return 1
+    else:
+        return 0
 
-y = 0
-parq11 = 0
-imparq11 = 0
-parq12 = 0
-imparq12 = 0
-while (y < 16):
-    if (matriz[y] != "x"):
-        if (y % 2 == 0):   
-            if (int(matriz[y]) % 2 == 0):
-                parq11 += 1
-            else:
-                imparq11 += 1
+def criarQ2(binario): #verifica o bit necessário para as colunas 3 e 4
+    y = 2
+    um = 0
+    while (y < 16): 
+        if (binario[y] != "x"):  
+            if (binario[y] == "1"):
+                um += 1
+        if (y % 2 == 0):
+            y += 1
         else:
-            if (int(matriz[y]) % 2 == 0):
-                parq12 += 1
-            else:
-                imparq12 += 1 
-    y += 1
-if (parq11 > imparq11):
-    matriz [2] = 0
-else:
-    matriz [2] = 1
-if (parq12 > imparq12):
-    matriz [1] = 0
-else:
-    matriz[1] = 1
-'''
+            y += 3
+    if (um % 2 == 1):
+        return 1
+    else:
+        return 0
+
+def criarQ3(binario): #verifica o bit necessário para as linhas 2 e 4
+    y = 4
+    um = 0
+    while (y < 16):
+        if (binario[y] != "x"):  
+            if (binario[y] == "1"):
+                um += 1
+        if (y == 7):
+            y = 12
+        else:
+            y += 1
+    if (um % 2 == 1):
+        return 1
+    else:
+        return 0
+
+def criarQ4(binario): #verifica o bit necessário para as linhas 3 e 4
+    y = 8
+    um = 0
+    while (y < 16):
+        if (binario[y] != "x"):
+            if (binario[y] == "1"):
+                um += 1
+        y += 1
+    if (um % 2 == 1):
+        return 1
+    else:
+        return 0
+
+def criarQ0(binario): #verifica o bit necessário para todas as linhas
+    y = 1
+    um = 0
+    while (y < 16): 
+        if (binario[y] == "1"):
+            um += 1
+        y += 1
+    if (um % 2 == 1):
+        return 1
+    else:
+        return 0
+
+for saida in conjuntos:
+    lista = list(saida)
+    lista[1] = str(criarQ1(saida))
+    lista[2] = str(criarQ2(saida))
+    lista[4] = str(criarQ3(saida))
+    lista[8] = str(criarQ4(saida))
+    lista[0] = str(criarQ0("".join(lista)))
+    saida = "".join(lista)
+    binario = binario + saida
+
+print("Binário com Hamming:", binario)
