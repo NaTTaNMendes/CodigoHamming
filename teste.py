22854ec2ef87474f33a89b8daaa0fbb49a8ba7c3
NaTTaNMendes <nattan078@gmail.com>
2022-07-08 11:39:34 -0300

    Aplicando conversao e tentando dividir os bits em grupos de bytes

--- teste.py
+++ teste.py
@@ -18,4 +18,20 @@ else:
     else:
         print('Nenhum erro encontrado')
 
-    
+#Desenvolvido por Prô Terra - MakerZine
+#Para mais detalhes, acesse: https://www.makerzine.com.br
+
+binario = int(input("Digite o número (binário) para ser convertido para a base decimal: "))
+n = len(str(binario))
+valor_digitado = binario
+decimal = 0
+i = 0
+
+while n >= 0:
+  resto = binario % 10
+  decimal = decimal + (resto * (2**i))
+  n = n - 1
+  i = i + 1
+  binario = binario // 10
+
+print("O número (binario) digitado",valor_digitado,", na base decimal, vale:",decimal)
